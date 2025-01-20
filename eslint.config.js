0ead64c69eccf0aaed740006f72d956152f05603
Jorge Ezequiel Fabbroni Assum <jorge.fabbroni@cabify.com>
2025-01-20 15:30:04 -0300

    fix lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 // /* eslint-disable import/extensions */
 // eslint-disable-next-line import/extensions
-import defaultRules from './recommended.js';
+import defaultRules from './configs/base.js';
 
 const globalIgnores = {
   ignores: ['dist', 'node_modules/*', 'build', 'scripts', 'vite.config.js'],
